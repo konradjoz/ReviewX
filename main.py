3d5dfaa5213e7c7370218cd49bf1ca09d5c01686
Konrad <konradjozefczyk2003@gmail.com>
2022-03-28 15:17:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from models import User, Review, Shop, test
+import datetime
+from mongoengine import connect
+from pymongo import MongoClient
+from dns import *
+import json
+
+app = FastAPI()
+connect(host="mongodb+srv://root:eauTEEvJzlzgHcmE@reviewapi-cluster.xb7wp.mongodb.net/ReviewX?retryWrites=true&w=majority")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/index", response_class=RedirectResponse)
+async def root(request: Request):
+    return templates.TemplateResponse('base.html', {'request': request})
+
+@app.get("/login/", response_class=HTMLResponse)
+async def get_login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login/", response_class=HTMLResponse)
+async def post_login(request: Request, username: str = Form(...), password: str = Form(...)):
+    if User.objects(username=username).count() == 1:
+        users = User.objects.get(username=username)
+        if users.password == password:
+            context = {"request": request, "user": users}
+            return RedirectResponse(url=f'/user/{username}')
+        else:
+            context = {
+                    "request": request,
+                    "login_status": True,
+                    "update_time": datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+                }
+            return templates.TemplateResponse("login.html", context)
+    else:
+        return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+@app.get("/user/{username}", response_class=HTMLResponse)
+async def user_profile(username: str, request: Request):
+    if User.objects(username=username).count() == 1:
+        users = User.objects.get(username=username)
+        return templates.TemplateResponse("test.html", {"request": request, "user": users})
+    else:
+        context = {
+            "request": request,
+            "username": username,
+            "update_time" : datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        }
+
+        return templates.TemplateResponse("error/user_not_found.html", context)
---
+++ models.py
@@ -0,0 +1,44 @@
+from mongoengine import Document, StringField, IntField, ListField, ReferenceField, DateTimeField, BooleanField, \
+    FloatField, EmbeddedDocument
+
+
+class Shop(Document):
+    meta = {'collection': 'Shop'}
+    name = StringField(required=True, max_length=50)
+    url = StringField(required=True, unique=True)
+    created_at = DateTimeField(required=True)
+    updated_at = DateTimeField(required=True)
+    total_reviews_count = IntField(required=True, default=0)
+    average_review = FloatField(required=True, default=0)
+    reviews = ListField(ReferenceField('Review'))
+
+
+class Review(EmbeddedDocument):
+    meta = {'collection': 'Review'}
+    shop = ReferenceField('Shop', required=True)
+    author = ReferenceField('User', required=True)
+    created_at = DateTimeField(required=True)
+    updated_at = DateTimeField(required=True)
+    content_title = StringField(required=True, max_length=50)
+    content = StringField(required=True, max_length=1000)
+    rating = IntField(required=True, min_value=1, max_value=5)
+    helpful_count = IntField(required=True, default=0)
+
+
+class User(Document):
+    meta = {'collection': 'User'}
+    user_id = IntField(required=True, unique=True)
+    username = StringField(required=True, unique=True, max_length=50)
+    password = StringField(required=True, min_length=8)
+    email = StringField(required=True, unique=True, max_length=50)
+    first_name = StringField(required=True, max_length=50)
+    last_name = StringField(required=True, max_length=50)
+    created_at = DateTimeField(required=True)
+    updated_at = DateTimeField(required=True)
+    is_admin = BooleanField(required=True, default=False)
+    is_active = BooleanField(required=True)
+
+class test(Document):
+    meta = {'collection': 'test'}
+    name = StringField(max_length=50)
+    name_id = StringField(unique=True)
